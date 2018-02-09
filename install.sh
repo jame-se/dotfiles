7a72608cc0fa808624db3d541963902a5e5f0da6
James Edwards <jedwards@bsdftw.org>
2018-02-08 23:17:45 -0500

    adding dotfiles

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+install -o `whoami` -g `id -gn` -m 0600 .cshrc ~/.cshrc
+install -o `whoami` -g `id -gn` -m 0600 .logout ~/.logout
+install -o `whoami` -g `id -gn` -m 0600 .vimrc ~/.vimrc
+install -o `whoami` -g `id -gn` -m 0600 .muttrc ~/.muttrc
+install -o `whoami` -g `id -gn` -m 0600 .screenrc ~/.screenrc
