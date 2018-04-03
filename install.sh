62dfbb3d2e4290d1986a978deecba3d5921da812
James Edwards <jedwards@bsdftw.org>
2018-04-03 13:20:07 -0400

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-install -o `whoami` -g `id -gn` -m 0600 .cshrc ~/.cshrc
-install -o `whoami` -g `id -gn` -m 0600 .logout ~/.logout
-install -o `whoami` -g `id -gn` -m 0600 .vimrc ~/.vimrc
-install -o `whoami` -g `id -gn` -m 0600 .muttrc ~/.muttrc
-install -o `whoami` -g `id -gn` -m 0600 .screenrc ~/.screenrc
+TMPDIR="/tmp/dotfiles"
+
+git clone git@github.com:j-e-d-w-a-r-d-s/dotfiles.git $TMPDIR
+
+install -o `whoami` -g `id -gn` -m 0600 $TMPDIR/.cshrc ~/.cshrc
+install -o `whoami` -g `id -gn` -m 0600 $TMPDIR/.logout ~/.logout
+install -o `whoami` -g `id -gn` -m 0600 $TMPDIR/.vimrc ~/.vimrc
+install -o `whoami` -g `id -gn` -m 0600 $TMPDIR/.muttrc ~/.muttrc
+install -o `whoami` -g `id -gn` -m 0600 $TMPDIR/.screenrc ~/.screenrc
+
+rm -rf /tmp/dotfiles
